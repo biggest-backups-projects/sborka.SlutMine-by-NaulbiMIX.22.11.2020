2276ff87aee7431edd8dc9816ae7e4157c0d7a91
MrVaylis <mrvaylis@icloud.com>
2025-01-27 00:59:34 +0300

    22.11.2020

---
+++ plugins/PlaceholderAPI/javascripts/boost.js
@@ -0,0 +1,7 @@
+var a = "%bungee_total%";
+
+function showMessag() {
+ return (Math.floor((a * 2)/1));
+}
+
+showMessag();
---
+++ plugins/PlaceholderAPI/javascripts/clan.js
@@ -0,0 +1,13 @@
+var faction = "%clans_prefix%";
+ 
+function check() {
+ 
+   if (faction === "") {
+       return "Нет";
+   }
+   
+   else {
+       return "%clans_prefix%";
+   }
+}
+check();
